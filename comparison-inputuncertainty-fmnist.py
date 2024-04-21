a236511c4c60f72c16096d6f53daeb6b8bfe0b43
Marco Zullich <marco.zullich@gmail.com>
2024-04-21 23:41:35 +0200

    added flipout and ensemble

--- comparison-inputuncertainty-fmnist.py
+++ comparison-inputuncertainty-fmnist.py
@@ -7,6 +7,11 @@ from tensorflow.keras.datasets import fashion_mnist
 
 import utils.preact_resnet18_bayes as pa18
 
+import argparse
+
+import logging
+
+
 def get_fmnist():
     (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
     x_train = np.expand_dims(x_train, axis=-1).astype(np.float32) / 255
@@ -19,27 +24,119 @@ def get_fmnist():
 def get_noise(shape, std:float):
     return np.random.randn(*shape) * std
 
-def train_standard_model(x_train_with_noise, y_train):
-    model = pa18.get_standard_preact_resnet18([28, 28, 1])
+def accuracy(y_pred, y_true):
+    return np.mean(np.argmax(y_pred, axis=1) == y_true)
+
+def train_standard_model(x_train_with_noise, y_train, x_test_with_noise, y_test, epochs=15, batch_size=256):
+    model = pa18.get_standard_preact_resnet18(x_train[0].shape)
     model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-    model.fit(x_train_with_noise, y_train, verbose=1, epochs=15, batch_size=256)
+    history = model.fit(x_train_with_noise, y_train, verbose=1, epochs=epochs, batch_size=batch_size)
+    train_accuracy = history.history["accuracy"][-1]
+
+    history = model.evaluate(x_test_with_noise, y_test)
+    test_accuracy = history[1]
+
+    logging.info(f"Standard, Train accuracy: {train_accuracy:.4f}, Test accuracy: {test_accuracy:.4f}")
+
     return model
 
-def train_dropout_model(x_train_with_noise, y_train):
-    model = pa18.get_dropout_preact_resnet18([28, 28, 1])
-    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-    model.fit(x_train_with_noise, y_train, verbose=1, epochs=15, batch_size=256)
+def train_dropout_model(x_train_with_noise, y_train, x_test_with_noise, y_test, epochs=15, batch_size=256):
+    model = pa18.get_dropout_preact_resnet18(x_train[0].shape)
+    history, model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    model.fit(x_train_with_noise, y_train, verbose=1, epochs=epochs, batch_size=batch_size)
     model_mc = StochasticClassifier(model, num_samples=25)
+
+    history = model.evaluate(x_test_with_noise, y_test)
+    test_accuracy = history[1]
+
+    logging.info(f"Dropout, Train accuracy: {train_accuracy:.4f}, Test accuracy: {test_accuracy:.4f}")
+
     return model_mc
+
+def train_dropconnect_model(x_train_with_noise, y_train, x_test_with_noise, y_test, epochs=15, batch_size=256):
+    model = pa18.get_dropconnect_preact_resnet18(x_train[0].shape)
+    history, model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    model.fit(x_train_with_noise, y_train, verbose=1, epochs=epochs, batch_size=batch_size)
+    model_mc = StochasticClassifier(model, num_samples=25)
+
+    history = model.evaluate(x_test_with_noise, y_test)
+    test_accuracy = history[1]
+
+    logging.info(f"DropConnect, Train accuracy: {train_accuracy:.4f}, Test accuracy: {test_accuracy:.4f}")
+
+    return model_mc
+
+def train_flipout_model(x_train_with_noise, y_train, x_test_with_noise, y_test, epochs=15, batch_size=256):
+    num_batches = len(x_train_with_noise) // batch_size + (0 if len(x_train_with_noise) % batch_size == 0 else 1)
+    model = pa18.get_flipout_preact_resnet18(x_train[0].shape, num_batches)
+    history, model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    model.fit(x_train_with_noise, y_train, verbose=1, epochs=epochs, batch_size=batch_size)
+    model_mc = StochasticClassifier(model, num_samples=25)
+
+    history = model.evaluate(x_test_with_noise, y_test)
+    test_accuracy = history[1]
+
+    logging.info(f"Filpout, Train accuracy: {train_accuracy:.4f}, Test accuracy: {test_accuracy:.4f}")
+
+    return model_mc
+
+def train_ensemble_model(x_train_with_noise, y_train, x_test_with_noise, y_test, epochs=15, batch_size=256):
+    def model_fn():
+        model = pa18.get_standard_preact_resnet18(x_train[0].shape)
+        model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+        return model
+    
+    ensemble = DeepEnsembleClassifier(model_fn, num_estimators=5)
+    history = ensemble.fit(x_train_with_noise, y_train, verbose=1, epochs=epochs, batch_size=batch_size)
+
+    
+    history = ensemble.evaluate(x_test_with_noise, y_test)
+    test_accuracy = history[1]
+    logging.info(f"Ensemble, Train accuracy: -- , Test accuracy: {test_accuracy:.4f}")
+    
+
+    
+
+
+
+STD_TRAIN = 0.1
+STD_TEST = [0.0, 0.1, 0.2, 0.3]
+
 
 
 if __name__ == "__main__":
+    # args = parse_args()
+    # print(args)
+
+    logging.basicConfig(
+        filename="logging.log",
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filemode="a"
+    )
+
     (x_train, x_test), (y_train, y_test) = get_fmnist()
-    for noise_level in (0.0, 0.05, 0.5):
-        print(f"### noise level {noise_level}###")
-        noise_train = get_noise(x_train.shape, noise_level)
-        noise_test = get_noise(x_test.shape, noise_level)
-        model = train_dropout_model((x_train, noise_train), y_train)
-        model.evaluate((x_test, noise_test), y_test)
+    noise_level = STD_TRAIN
+    noise_train = get_noise(x_train.shape, noise_level)
+    noise_test = get_noise(x_test.shape, noise_level)
+    
+    standard = train_standard_model((x_train, noise_train), y_train, (x_test, noise_test), y_test)
+    standard.save_weights("standard.weights")
+
+    dropout = train_dropout_model((x_train, noise_train), y_train, (x_test, noise_test), y_test)
+    dropout.save_weights("dropout.weights")
+
+    dropconn = train_dropconnect_model((x_train, noise_train), y_train, (x_test, noise_test), y_test)
+    dropconn.save_weights("dropconn.weights")
+
+    flip = train_flipout_model((x_train, noise_train), y_train, (x_test, noise_test), y_test)
+    flip.save_weights("filpout.weights")
+
+    ensemble = train_ensemble_model((x_train, noise_train), y_train, (x_test, noise_test), y_test)
+    ensemble.save_weights("ensemble.weights")
+
+
+    
     
 
--- utils/preact_resnet18_bayes.py
+++ utils/preact_resnet18_bayes.py
@@ -3,6 +3,8 @@ from utils.preact_resnet18 import two_inputs_preact_resnet18
 from keras.layers import Conv2D
 from keras_uncertainty.layers import DropConnectConv2D, VariationalConv2D, FlipoutConv2D, StochasticDropout
 
+from typing import List
+
 class DropoutConv2D(Conv2D):
     def __init__(self, filters, kernel_size, p_drop:float, **kwargs):
         super().__init__(filters=filters, kernel_size=kernel_size, **kwargs)
@@ -20,5 +22,19 @@ def get_standard_preact_resnet18(input_shape, num_classes:int=10):
 def get_dropout_preact_resnet18(input_shape, num_classes:int=10, p_drop=0.1):
     return two_inputs_preact_resnet18(input_shape, lambda filters, kernel_size, strides, padding : DropoutConv2D(filters, strides=strides, kernel_size=kernel_size, padding=padding, p_drop=p_drop), num_classes)
 
-def get_dropconnect_preact_resnet18(num_classes:int=10):
-    pass
+def get_dropconnect_preact_resnet18(input_shape, num_classes:int=10, p_drop:float=0.05):
+    return two_inputs_preact_resnet18(input_shape, lambda filters, kernel_size, strides, padding : DropConnectConv2D(filters, strides=strides, kernel_size=kernel_size, padding=padding, prob=p_drop), num_classes)
+
+def get_flipout_preact_resnet18(input_shape, num_batches:int, num_classes:int=10):
+    kl_weight = 1.0 / num_batches
+    return two_inputs_preact_resnet18(input_shape, lambda filters, kernel_size, strides, padding : VariationalConv2D(filters, strides=strides, kernel_size=kernel_size, padding=padding, kl_weight=kl_weight), num_classes)
+
+# def get_ensemble_preact_resnet18(input_shape, n_components:int, loss:str="categorical_crossentropy", optimizer:str="adam", metrics:List[str]=["accuracy"], num_classes:int=10):
+#     def model_fn():
+#         model = two_inputs_preact_resnet18(input_shape, Conv2D, num_classes)
+#         model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+#         return model
+    
+#     ensemble = DeepEnsembleClassifier(model_fn, num_estimators=n_components)
+#     return ensemble
+
